22cf5dda26895e92e2c2e03dffc7777529cdc7ca
Jesús Sánchez <jsbertolin@gmail.com>
2019-11-14 15:52:21 +0100

    Creación proyecto Discoteca

---
+++ Java/Discoteca/src/main/java/com/geekshubs/proyecto/discoteca/DiscotecaApplication.java
@@ -0,0 +1,13 @@
+package com.geekshubs.proyecto.discoteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscotecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscotecaApplication.class, args);
+    }
+
+}
---
+++ Java/Discoteca/src/test/java/com/geekshubs/proyecto/discoteca/DiscotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekshubs.proyecto.discoteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscotecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
