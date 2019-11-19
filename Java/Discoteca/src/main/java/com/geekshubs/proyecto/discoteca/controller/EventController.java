76cb669f8c65ac26190d9713eb353b89635a92a1
Jesús Sánchez <jsbertolin@gmail.com>
2019-11-19 14:01:10 +0100

    Reorganizar proyecto, añadir variabe sesion para superadmin y template error devuelta a usuarios no logueados

--- Java/Discoteca/src/main/java/com/geekshubs/proyecto/discoteca/controller/EventController.java
+++ Java/Discoteca/src/main/java/com/geekshubs/proyecto/discoteca/controller/EventController.java
@@ -5,21 +5,29 @@ import com.geekshubs.proyecto.discoteca.model.entities.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 @RequestMapping("/events")
 public class EventController {
 
+    public static final String ERROR_TITLE_SUPERADMIN = "You are not a superadmin";
+    public static final String ERROR_MSG_SUPERADMIN = "You cannot create/edit/remove events if you're not logged in as superadmin";
+
     @Autowired
     private IEventDAO eventDAO;
 
     @GetMapping("/list")
-    public String listEvents(@RequestParam(required = false, defaultValue="") String overviewLike, Model model) {
+    public ModelAndView listEvents(@RequestParam(required = false, defaultValue="") String overviewLike, Model model) {
         List<Event> eventList;
         boolean isFiltered = false;
         if(overviewLike.isEmpty())
@@ -33,13 +41,65 @@ public class EventController {
         model.addAttribute("overviewLike", overviewLike);
         model.addAttribute("eventList", eventList);
         model.addAttribute("isFiltered", isFiltered);
-        return "events/list";
+        return new ModelAndView("events/list");
     }
 
     @GetMapping("/details")
-    public String listEvents(@RequestParam Long eventId, Model model) {
+    public ModelAndView listEvents(@RequestParam Long eventId, Model model) {
         model.addAttribute("title", "Event details");
         model.addAttribute("event", eventDAO.findEventById(eventId));
-        return "events/details";
+        return new ModelAndView("events/details");
+    }
+
+
+    // SOLO PARA SUPERADMIN
+    @GetMapping("/create")
+    public ModelAndView createEvent(Model model, HttpSession session) {
+        if((boolean) session.getAttribute("superadminLogged")) {
+            model.addAttribute(new Event());
+            model.addAttribute("title", "Create an event");
+            return new ModelAndView("events/form_new_edit");
+        }
+        else
+            return new ModelAndView("redirect:/error?errorTitle="+ERROR_TITLE_SUPERADMIN+"&errorMessage="+ERROR_MSG_SUPERADMIN);
+    }
+
+    @GetMapping("/edit")
+    public ModelAndView editEvent(@RequestParam Long eventId, Model model, HttpSession session) {
+        if((boolean) session.getAttribute("superadminLogged")) {
+            model.addAttribute("event", eventDAO.findEventById(eventId));
+            model.addAttribute("title", "Edit an event");
+            return new ModelAndView("events/form_new_edit");
+        }
+        else
+            return new ModelAndView("redirect:/error?errorTitle="+ERROR_TITLE_SUPERADMIN+"&errorMessage="+ERROR_MSG_SUPERADMIN);
+    }
+
+    @PostMapping("/store")
+    public ModelAndView storeEvent(@Valid Event event, BindingResult result, Model model, HttpSession session) {
+        if((boolean) session.getAttribute("superadminLogged")) {
+            if (result.hasErrors()) {
+                model.addAttribute("title", "Create an event");
+                return new ModelAndView("events/form_new_edit");
+            }
+            if (event.getId() != null)
+                eventDAO.updateEvent(event);
+            else
+                eventDAO.insertEvent(event);
+
+            return new ModelAndView("redirect:create-event");
+        }
+        else
+            return new ModelAndView("redirect:/error?errorTitle="+ERROR_TITLE_SUPERADMIN+"&errorMessage="+ERROR_MSG_SUPERADMIN);
+    }
+
+    @PostMapping("/delete")
+    public ModelAndView delete(@RequestParam Long eventId, Model model, HttpSession session) {
+        if((boolean) session.getAttribute("superadminLogged")) {
+            eventDAO.deleteEventById(eventId);
+            return new ModelAndView("redirect:/events/list");
+        }
+        else
+            return new ModelAndView("redirect:/error?errorTitle="+ERROR_TITLE_SUPERADMIN+"&errorMessage="+ERROR_MSG_SUPERADMIN);
     }
 }
--- Java/Discoteca/src/main/java/com/geekshubs/proyecto/discoteca/controller/SuperAdminController.java
+++ Java/Discoteca/src/main/java/com/geekshubs/proyecto/discoteca/controller/SuperAdminController.java
@@ -1,17 +1,12 @@
 package com.geekshubs.proyecto.discoteca.controller;
 
 import com.geekshubs.proyecto.discoteca.model.dao.interfaces.IEventDAO;
-import com.geekshubs.proyecto.discoteca.model.entities.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.validation.Valid;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/superadmin")
@@ -20,36 +15,15 @@ public class SuperAdminController {
     @Autowired
     private IEventDAO eventDAO;
 
-    @GetMapping("/create-event")
-    public String createEvent(Model model) {
-        model.addAttribute(new Event());
-        model.addAttribute("title", "Create an event");
-        return "events/form_new_edit";
+    @GetMapping("/login")
+    public String login(HttpSession session) {
+        session.setAttribute("superadminLogged", true);
+        return "superadmin/menu";
     }
 
-    @GetMapping("/edit-event")
-    public String editEvent(@RequestParam Long eventId, Model model) {
-        model.addAttribute("event", eventDAO.findEventById(eventId));
-        return "events/form_new_edit";
-    }
-
-    @PostMapping("/store-event")
-    public String storeEvent(@Valid Event event, BindingResult result, Model model) {
-        if(result.hasErrors()) {
-            model.addAttribute("title", "Create an event");
-            return "events/form_new_edit";
-        }
-        if(event.getId() != null)
-            eventDAO.updateEvent(event);
-        else
-            eventDAO.insertEvent(event);
-        return "redirect:create-event";
-    }
-
-    // CAMBIAR POR POST
-    @GetMapping("/delete-event")
-    public String delete(@RequestParam Long eventId) {
-        eventDAO.deleteEventById(eventId);
-        return "redirect:/events/list";
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.setAttribute("superadminLogged", false);
+        return "superadmin/menu";
     }
 }
